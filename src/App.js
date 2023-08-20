01bb87317fd01b99c28d1204c83ff9c73696ed89
seongho <tjdghgk98@gmail.com>
2023-08-20 22:34:44 +0900

    feat : history 관리 -> App에서 하도록 - 배열을 활용해 히스토리 관리를 했음

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
 import "./App.css"
 import Board from "./components/Board";
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+  const [xIsNext, setXIsNext] = useState(true);
+  
+  const calculateWiner = (squares) => {
+    const winList = [
+       [0,1,2],
+       [3,4,5],
+       [6,7,8],
+       [0,3,6],
+       [1,4,7],
+       [2,5,8],
+       [0,4,8],
+       [2,4,6]
+    ]
+
+    for (let index = 0; index < winList.length; index++) {
+       const [a,b,c] = winList[index];
+
+       if(squares[a] && squares[a] === squares[b]
+           && squares[b] === squares[c])
+       {
+          return squares[a]; //승자 리턴
+       }
+    }      
+    return null;
+ }
+
+ const current = history[history.length - 1];
+ const winner = calculateWiner(current.squares);
+
+ let status = `Next Player ${xIsNext ? 'X' : 'O'}`;   
+
+   if(winner){
+      status = `Winner: ${winner}`;
+   }else{
+      status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+   }
+
+  const handleClick = (i) => {
+      const newSquares = current.squares.slice();
+
+      if(calculateWiner(newSquares) || newSquares[i]){
+        return;
+      }
+
+      newSquares[i] = xIsNext? 'X' : 'O';
+      
+      setHistory([...history, {squares : newSquares}]);
+      setXIsNext(prev => !prev);
+   }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)}/>
       </div>
       <div className="game-info">
-        game-info
+        <div className="status">{status}</div>
       </div>
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -5,75 +5,17 @@ import React, { useState } from 'react'
 import Square from './Square'
 import "./Board.css";
 
-const Board = () => {
-   const [squares, setSquares] = useState(Array(9).fill(null))
-   const [xIsNext, setXIsNext] = useState(true);
+const Board = ({squares, onClick}) => {
 
-   let status = `Next Player ${xIsNext ? 'X' : 'O'}`;   
-
-   const calculateWiner = (squares) => {
-      const winList = [
-         [0,1,2],
-         [3,4,5],
-         [6,7,8],
-         [0,3,6],
-         [1,4,7],
-         [2,5,8],
-         [0,4,8],
-         [2,4,6]
-      ]
-
-      for (let index = 0; index < winList.length; index++) {
-         const [a,b,c] = winList[index];
-
-         if(squares[a] && squares[a] === squares[b]
-             && squares[b] === squares[c])
-         {
-            return squares[a]; //승자 리턴
-         }
-      }      
-      return null;
-   }
-
-   const winner = calculateWiner(squares);
-
-   if(winner){
-      status = `Winner: ${winner}`;
-   }else{
-      status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-   }
-
-   const handleClick = (i) => {
-
-      const newSquares = squares.slice();//state에 있는 것 복사
-
-      console.log('newSquares', newSquares);
-      console.log('newSquares[i]', newSquares[i]);
-      //winner가 있거나, 클릭이 이미 되어있다면
-      if(calculateWiner(newSquares) || newSquares[i]){
-         return; //클릭이 안되도록
-      }
-
-      newSquares[i] = xIsNext ? 'X': 'O';
-
-      setSquares(newSquares); //squares 자체를 다시 끼움
-      
-      setXIsNext(!xIsNext)
-      //setXIsNext(previousStateMM => !previousStateMM);
-      //비슷하게 동작하지만.. 좀 다른 부분이 있음 -> 생략
-      
-   }
-   
    const renderSquare = (i) => {
       return <Square valueM={squares[i]}
-            onClick={() => handleClick(i)}/>
+            onClick={() => onClick(i)}/>
 
       // props로 함수까지 내려줄 수 있음
     }
 
     return (
       <div>
-         <div className='status'>{status}</div>
          <div className='board-row'>
             {/* <Square />
              이렇게 그냥 사용해도 됨 */}
